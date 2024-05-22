990351b5affa62f889fd91ebff6a56e9ad275ebb
stevethomas <steve@codinglabs.io>
2024-05-22 11:53:21 +1000

    add parallel support

--- src/helpers.php
+++ src/helpers.php
@@ -4,11 +4,13 @@ namespace CodingLabs;
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 function styles(Finder $finder, array $rules = []): Config {
     $rules = array_merge(require __DIR__.'/rules.php', $rules);
 
     return (new Config())
+        ->setParallelConfig(ParallelConfigFactory::detect())
         ->setFinder($finder)
         ->setRiskyAllowed(true)
         ->setRules($rules);
