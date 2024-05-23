e97706fc1fce999fc13d8b9450ca5ee8318fe0ca
stevethomas <steve@codinglabs.io>
2024-05-23 14:35:34 +1000

    wip it

--- src/helpers.php
+++ src/helpers.php
@@ -10,7 +10,7 @@ function styles(Finder $finder, array $rules = []): Config {
     $rules = array_merge(require __DIR__.'/rules.php', $rules);
 
     return (new Config())
-        ->setParallelConfig(ParallelConfigFactory::detect())
+//        ->setParallelConfig(ParallelConfigFactory::detect()) // todo: restore this once it becomes stable
         ->setFinder($finder)
         ->setRiskyAllowed(true)
         ->setRules($rules);
