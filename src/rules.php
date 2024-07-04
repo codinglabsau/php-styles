be7c75ac9314a312195afe2c262698164eb16ed7
stevethomas <steve@codinglabs.io>
2024-07-05 09:11:56 +1000

    order functions last

--- src/rules.php
+++ src/rules.php
@@ -5,7 +5,7 @@ return [
     'array_syntax' => ['syntax' => 'short'],
     'final_class' => false,
     'new_with_braces' => true,
-    'ordered_imports' => ['sort_algorithm' => 'length'],
+    'ordered_imports' => ['sort_algorithm' => 'length', 'imports_order' => ['const', 'class', 'function']],
     'concat_space' => ['spacing' => 'one'],
     'single_line_comment_spacing' => true,
     'no_unused_imports' => true,
